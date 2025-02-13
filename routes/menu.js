7d5241d0210085dbfd622f5a581715abfeb6940c
annasybingco <sybingcoanna@gmail.com>
2025-02-13 12:45:16 -0500

    add route for menu item id

--- routes/menu.js
+++ routes/menu.js
@@ -3,10 +3,22 @@ import fs from "fs";
 const router = express.Router();
 
 const data = fs.readFileSync("./data/menu.json", "utf8");
-const photos = JSON.parse(data);
+const menu = JSON.parse(data);
 
 router.get("/", function (req, res) {
-  res.send(photos);
+  res.send(menu);
+});
+
+router.get("/:id", (req, res) => {
+  const menuItem = menu.find(
+    (menuItem) => menuItem.id === Number(req.params.id)
+  );
+
+  if (!menuItem) {
+    return res.status(404).json({ error: "Menu item not found" });
+  }
+
+  res.json(menuItem);
 });
 
 export default router;
